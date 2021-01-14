fac6941917a462924b342ec5899792d04759cdfd
Tom Schneider <52013041+ric03@users.noreply.github.com>
2021-01-14 02:09:41 +0100

    feat(main): setup and use new components

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,30 @@
 #include "Arduino.h"
-#include <driver/adc.h> //  ADC configuration
-#include <array>
+// #include <driver/adc.h> //  ADC configuration
 
 #include "dht_sensor.h"
+#include "oled_display.h"
+#include "button.h"
 
 // https://senseair.com/products/size-counts/s8-lp/ Product No. 004-0-0053
 // Senseair Sensor UART pins
 #define RXD2 13
 #define TXD2 12
 
-// SSD1306 Display i2c pins
-#define DPORT 0x3c
-#define DSDA 4
-#define DSCL 15
-#define DRST 16
-
 void setup()
 {
   Serial.begin(115200);
+
+  button::setup();
   dht_sensor::setup();
+  display::setup();
 }
 
-unsigned long nextTime = millis();
 void loop()
 {
   delay(2000);
 
-  dht_sensor::Data data = dht_sensor::getData();
+  auto data = dht_sensor::getData();
   dht_sensor::serialPrint(data);
+
+  Serial.printf("btn1 = %d | btn2 = %d \n", button::isBtn1Pressed(), button::isBtn2Pressed());
 }
