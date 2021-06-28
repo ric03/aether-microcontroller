97674bf255e320f1b5a5048f85f34ca2966347a4
Tom Schneider <52013041+ric03@users.noreply.github.com>
2021-06-28 02:29:29 +0200

    refactor: delete unnecessary `remove.sh`
    The `compose down` command already stops and removes the  containers, which makes the script `remove.sh` unnecessary.

--- container/stop.sh
+++ container/stop.sh
@@ -3,6 +3,16 @@ set -e
 
 PROJECT_NAME=aether
 
-docker compose --project-name $PROJECT_NAME down
+PARTIAL_VOLUME_NAME=influxdb-data
+VOLUME_NAME=${PROJECT_NAME}_${PARTIAL_VOLUME_NAME}
 
-echo 'containers are stopped. If you want to remove them run: ./remove.sh'
+echo 'Volumes are persistent and are not destroyed automatically.'
+read -p "Would you like to remove the volume $VOLUME_NAME (y/N)? " -n 1
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    # docker volume rm $VOLUME_NAME
+    REMOVE_VOLUMES=--volumes
+fi
+
+docker compose --project-name $PROJECT_NAME down $REMOVE_VOLUMES
